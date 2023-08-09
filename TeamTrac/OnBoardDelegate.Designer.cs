2a655baf2ad5b13bceb35ca2f252857efca7956b
Adir <adir.jscode@gmail.com>
2023-08-09 22:52:12 +0600

    Added Delagates field to register

--- TeamTrac/OnBoardDelegate.Designer.cs
+++ TeamTrac/OnBoardDelegate.Designer.cs
@@ -32,6 +32,20 @@
             pictureBox1 = new PictureBox();
             panel1 = new Panel();
             label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label9 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panel1.SuspendLayout();
             SuspendLayout();
@@ -41,13 +55,27 @@
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.Location = new Point(12, 88);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(500, 525);
+            pictureBox1.Size = new Size(526, 550);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
             // panel1
             // 
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(dateTimePicker2);
+            panel1.Controls.Add(dateTimePicker1);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
             panel1.Controls.Add(label1);
             panel1.Font = new Font("Cambria", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
             panel1.Location = new Point(544, 52);
@@ -59,11 +87,124 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Cambria", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(197, 36);
+            label1.Location = new Point(193, 12);
             label1.Name = "label1";
             label1.Size = new Size(234, 32);
             label1.TabIndex = 0;
             label1.Text = "OnBoard Delegates";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(20, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(130, 22);
+            label2.TabIndex = 1;
+            label2.Text = "Delegate Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(20, 155);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 22);
+            label3.TabIndex = 2;
+            label3.Text = "Phone Number";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(20, 213);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 22);
+            label4.TabIndex = 3;
+            label4.Text = "Email";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(20, 277);
+            label5.Name = "label5";
+            label5.Size = new Size(42, 22);
+            label5.TabIndex = 4;
+            label5.Text = "NID";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(20, 342);
+            label6.Name = "label6";
+            label6.Size = new Size(47, 22);
+            label6.TabIndex = 5;
+            label6.Text = "DOB";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(20, 403);
+            label7.Name = "label7";
+            label7.Size = new Size(108, 22);
+            label7.TabIndex = 6;
+            label7.Text = "Joining Date";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(20, 511);
+            label9.Name = "label9";
+            label9.Size = new Size(0, 22);
+            label9.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(156, 93);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(310, 30);
+            textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(156, 155);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(310, 30);
+            textBox2.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(156, 213);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(310, 30);
+            textBox3.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(156, 269);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(310, 30);
+            textBox4.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(156, 336);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(310, 30);
+            dateTimePicker1.TabIndex = 14;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(156, 395);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(310, 30);
+            dateTimePicker2.TabIndex = 15;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(237, 465);
+            button1.Name = "button1";
+            button1.Size = new Size(136, 46);
+            button1.TabIndex = 17;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
             // 
             // OnBoardDelegate
             // 
@@ -85,5 +226,19 @@
         private PictureBox pictureBox1;
         private Panel panel1;
         private Label label1;
+        private Label label9;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
