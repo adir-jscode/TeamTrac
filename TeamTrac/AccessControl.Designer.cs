d9a69358f61509365b03a7b6aa9d3248434e5da4
Adir <adir.jscode@gmail.com>
2023-08-16 00:59:52 +0600

    Access,Product Req formed

---
+++ TeamTrac/AccessControl.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace TeamTrac
+{
+    partial class AccessControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // AccessControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1189, 641);
+            Name = "AccessControl";
+            Text = "AccessControl";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
